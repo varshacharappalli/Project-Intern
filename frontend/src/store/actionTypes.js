a053a0f95a48dc3d5b20aab2da10b44424d00ee8
varshacharappalli <167790510+varshacharappalli@users.noreply.github.com>
2025-04-01 17:47:55 +0530

    commit

---
+++ frontend/src/store/actionTypes.js
@@ -0,0 +1,4 @@
+export const ADD_GSTIN = "ADD_GSTIN";
+export const ADD_GSTIN_ERROR = "ADD_GSTIN_ERROR";
+export const VIEW_GSTINS = "VIEW_GSTINS";
+export const VIEW_GSTINS_ERROR = "VIEW_GSTINS_ERROR";
